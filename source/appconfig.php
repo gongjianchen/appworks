c484607d2819b3bb6bc2cd429e5d26beba50454b
gongjianchen <gongjianchen@yahoo.ca>
2019-01-25 02:08:12 -0500

    Create appconfig.php

---
+++ source/appconfig.php
@@ -0,0 +1,13 @@
+<?php 
+
+//MySQL Database:
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'labuser');
+define ('DB_PASS', 'winterfell');
+define ('DB_NAME', 'thedb');
+
+//Integration Keys
+$apiKey = '1234567890';
+$apiSecret = 'abcdefghilji';
+
+?>
